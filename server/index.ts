c067ef766ce513200d2fd4cbf49db15580ef24be
personalizefdsj-lab <personalize.fdsj@gmail.com>
2026-03-17 14:08:56 -0300

    Comment out registerRoutes call in index.ts

--- server/index.ts
+++ server/index.ts
@@ -70,7 +70,7 @@ app.use((req, res, next) => {
 
 (async () => {
   //await seedDatabase().catch(console.error);
-  await registerRoutes(httpServer, app);
+  //await registerRoutes(httpServer, app);
 
  app.use((err: any, _req: Request, res: Response, next: NextFunction) => {
     const status = err.status || err.statusCode || 500;
